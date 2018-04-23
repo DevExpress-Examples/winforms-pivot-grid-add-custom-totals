6e9272a3010ba069a4e7a12077f74218c6e61f2f
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/HowToAddCustomTotals/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Data;
+using System.Data.OleDb;
+using System.Windows;
+using DevExpress.Xpf.PivotGrid;
+using HowToBindToMDB.NwindDataSetTableAdapters;
+using System;
+
+namespace HowToBindToMDB {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        NwindDataSet.SalesPersonDataTable salesPersonDataTable = new NwindDataSet.SalesPersonDataTable();
+        SalesPersonTableAdapter salesPersonDataAdapter = new SalesPersonTableAdapter();
+
+        public MainWindow() {
+            InitializeComponent();
+            pivotGridControl1.DataSource = salesPersonDataTable;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+            salesPersonDataAdapter.Fill(salesPersonDataTable);
+            object[] filter = new object[15];
+            Array.Copy(fieldProductName.GetUniqueValues(), filter, filter.Length);
+            fieldProductName.FilterValues.ValuesIncluded = filter;
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,13 @@
+# How to: Add Custom Totals
+
+
+<p>The following example demonstrates how to add custom totals for a particular pivot grid field.</p>
+
+
+<h3>Description</h3>
+
+<p>In this example, four different totals are added for the <strong>Category Name</strong> row field: Average, Sum, Min and Max. For this, they should be added to the <strong>PivotGridField.CustomTotals</strong> property and the <strong>PivotGridField.TotalsVisibility</strong> property should be set to <strong>FieldTotalsVisibility.CustomTotals</strong>.</p>
+
+<br/>
+
+
